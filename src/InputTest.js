9803c9d91e18e8e5c1fe3ea584c5e0a296cf8d2a
Rob <klassen.rob@gmail.com>
2021-10-08 20:11:40 -0400

    playing with linhs code

--- src/InputTest.js
+++ src/InputTest.js
@@ -1,43 +1,51 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const InputTest = () => {
-  const [searchValue, setSearchValue] = useState("");
+    const [searchValue, setSearchValue] = useState("");
+    // const [searchValue, setSearchValue] = useState("harry potter");
+    const [movieResults, setMovieResults] = useState([]);
 
-  
-
-  const makeQuery = () => {
-    
-    axios({
+    //useeffect instead of a function
+    useEffect(function(){
+        //if statement just checks if there's a value in searchvalue, if there isn't then it doesn't make a call (the api hates empty strings)
+        if (searchValue){
+            axios({
                 url: `https://api.themoviedb.org/3/search/movie/`,
                 method: "GET",
                 dataResponse: "json",
                 params: {
                     api_key: "686f4b568f616a8066ae90d21c06dffe",
                     query: searchValue
-            },
+                },
             }).then((res) => {
-                console.log(searchValue);
-                let movieResults = res.data.results;
-                console.log(movieResults);
+                console.log(res.data.results);
+                setMovieResults(res.data.results)
             });
-  }
-
+        }
+        //end if
+    //listens for a searchvalue call
+    },[searchValue]);
   
-  const getSearch = (e) => {
-    const inputVal = e;
-    setSearchValue(inputVal);
-    makeQuery();
-  }
+    //unaltered, except I removed the makeQuery call
+    const getSearch = (e) => {
+        const inputVal = e;
+        setSearchValue(inputVal);
+    }
 
+    //bestMatch() looks for the first result from the query so far when we hit submit.
+    const bestMatch = function(e){
+        e.preventDefault();
+        console.log(movieResults[0].id);
+        // console.log(movieResults[0]);
+    }
 
-  return(
-    <form onSubmit={makeQuery}>
-      <input type="text" value={searchValue} onChange={(e) => getSearch(e.target.value)} />
-      <button>gogogogo</button>
-    </form>
-  )
+    return(
+        <form onSubmit={ bestMatch }>
+            <input type="text" value={searchValue} onChange={(e) => getSearch(e.target.value)} />
+            <button>gogogogo</button>
+        </form>
+    )
 }
-
 
 export default InputTest;
