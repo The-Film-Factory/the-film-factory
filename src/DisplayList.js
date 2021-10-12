84e01efedd46373bffef04d668dfd17cdbf2ea03
danielnoo <dannynoo@gmail.com>
2021-10-11 22:35:16 -0400

    took movie match data from diplayList.js and sent to component moviePair.js to display each of the paired movie cards

--- src/DisplayList.js
+++ src/DisplayList.js
@@ -1,6 +1,8 @@
 import { handleDisplayData } from "./ourDbFunctions";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import MoviePair from './MoviePair';
+
 
 const DisplayList = () => {
   const [matchList, setMatchList] = useState([]);
@@ -19,49 +21,24 @@ const DisplayList = () => {
     });
   }, []);
 
-  useEffect(() => {
-    // to allow us to write asynchronous logic
-    async function apiCallID(URL) {
-      const dbCall = await axios({
-        url: URL,
-        method: "GET",
-        dataResponse: "json",
-        params: {
-          api_key: "686f4b568f616a8066ae90d21c06dffe",
-        },
-      });
-      const dbResults = await dbCall;
-      console.log(dbResults);
-      return dbResults;
-    }
-
-    // create empty array and fill array with movies (promises)
-    let newArray = [];
-
-    for (let i = 0; i < matchList.length; i++) {
-      newArray.push(
-        apiCallID(
-          `https://api.themoviedb.org/3/movie/${matchList[i].foreignMovie}`
-        )
-      );
-    }
-    console.log(newArray);
-    // setMovieDB(newArray);
-
-    // callback to run when all promises are fulfilled (once movies have returned)
-    // Promise.all(newArray).then((allResponses) => {
-    //   allResponses.forEach((response) => {
-    //     // console.log(response);
-    //     promiseArray.push(response.data.results);
-    //   });
-
-    //   setMovieDB(promiseArray);
-    // });
-  }, [matchList]);
+  
 
   return (
+    
+    
     <div>
-      <p></p>
+      { matchList ?
+      
+      
+      matchList.map((match) => {
+        return(
+          <MoviePair key={match.id} match={match} />
+        )
+      })
+
+      : null
+      
+      }
     </div>
   );
 };
