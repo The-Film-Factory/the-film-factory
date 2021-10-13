008066442b2b74117d9dad0f5d806bda8e30f05b
danielnoo <dannynoo@gmail.com>
2021-10-12 23:25:00 -0400

    basic pagination functionality working, needs cleanup

--- src/DisplayList.js
+++ src/DisplayList.js
@@ -6,8 +6,8 @@ import MoviePair from './MoviePair';
 
 const DisplayList = () => {
   const [matchList, setMatchList] = useState([]);
-  const [movieDB, setMovieDB] = useState([]);
-
+  const [pageNum, setPageNum] = useState(0)
+  const [nextPage, setNextPage] = useState([])
   useEffect(() => {
     handleDisplayData((snapshot) => {
       const data = snapshot.val();
@@ -16,27 +16,55 @@ const DisplayList = () => {
       for (let key in data) {
         dbArray.push(data[key]);
       }
-
+      // reverse the array of db results so that 
+      // recent results show at the top of the list
+      dbArray.reverse();
       setMatchList(dbArray);
     });
   }, []);
 
+  const handleClick = () => {
+    
+    
+    setPageNum(pageNum + 1)
+    
+    const sliceStart = pageNum + 10;
+    const sliceEnd = sliceStart + 10;
+
+    setNextPage(matchList.slice(sliceStart, sliceEnd));
   
+  }
 
   return (
     
     
     <div className="displayPairs">
-      { matchList ?
+      { matchList.length > 0 && pageNum === 0 ?
       
-      
-      matchList.map((match) => {
-        return(
-          <MoviePair key={match.id} match={match} />
-        )
-      })
+        matchList.slice(0, 10).map((match) => {
+          return(
+            <MoviePair key={match.id} match={match} />
+          )
+        })
 
-      : null
+        : <h3>API is loading!</h3>
+      
+      }
+
+
+      
+        
+      <button onClick={handleClick}>next page</button> 
+        
+      { pageNum > 0 ? 
+
+        nextPage.map((match) => {
+          return(
+            <MoviePair key={match.id} match={match} />
+          )
+        })
+
+        : <h3>API is loading!</h3>
       
       }
     </div>
@@ -44,3 +72,4 @@ const DisplayList = () => {
 };
 
 export default DisplayList;
+      
