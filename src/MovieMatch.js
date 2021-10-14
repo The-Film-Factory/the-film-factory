d57f84bde5f93652a4a2f4f52fa9bda69e042a15
danielnoo <dannynoo@gmail.com>
2021-10-14 15:38:24 -0400

    fixed moviematch bug

--- src/MovieMatch.js
+++ src/MovieMatch.js
@@ -39,52 +39,53 @@ const MovieMatch = () => {
     [movieID, foreignMovieID]
   );
 
-  return viewMatch === true ? (
-    <div className="matchedMovieSection">
-      <h3>You've made a recommendation!</h3>
-      <ul className="matchedMovieContainer">
-        <MovieCard
-          key={englishFilmRes.id}
-          movieKey={englishFilmRes.id}
-          cardClass={"movieMatchEnglishCard"}
-          imgClass={"imgContainer"}
-          movieOgLang={englishFilmRes.original_language}
-          movieTitle={englishFilmRes.title}
-          moviePoster={englishFilmRes.poster_path}
-        />
+  return (
+    viewMatch === true ?
+      <div className="matchedMovieSection">
+        <h3>You've made a recommendation!</h3>
+        <ul className="matchedMovieContainer">
+          <MovieCard
+            key={englishFilmRes.id}
+            movieKey={englishFilmRes.id}
+            cardClass={"movieMatchEnglishCard"}
+            imgClass={"imgContainer"}
+            movieOgLang={englishFilmRes.original_language}
+            movieTitle={englishFilmRes.title}
+            moviePoster={englishFilmRes.poster_path}
+          />
 
-        {/* push englishMovieID and foreignMovieID into firebase */}
-        <button
-          onClick={() => {
-            handlePushToFirebase(movieID, foreignMovieID);
-            setViewMatch(false);
-          }}
-        >
-          Save as a public match
-        </button>
+          {/* push englishMovieID and foreignMovieID into firebase */}
+          <button
+            onClick={() => {
+              handlePushToFirebase(movieID, foreignMovieID);
+              setViewMatch(false);
+            }}
+          >
+            Save as a public match
+          </button>
 
-        <MovieCard
-          key={foreignFilmRes.id}
-          movieKey={foreignFilmRes.id}
-          cardClass={"movieMatchForeignCard"}
-          imgClass={"imgContainer"}
-          movieOgLang={foreignFilmRes.original_language}
-          movieTitle={foreignFilmRes.title}
-          moviePoster={foreignFilmRes.poster_path}
-        />
-      </ul>
-    </div>
-  ) : (
-      <h3>Saved!</h3>
-            </div>
-            :
-            <>
-              <h3>Saved!</h3>
-              
-              <Link className="linkButton" to={`/movie/${movieID}`}>Choose another recommendation for this film</Link>
-              <Link className="linkButton" to={'/watchlist'}>View this match among others in the public match list</Link>
-    
-            </>
+          <MovieCard
+            key={foreignFilmRes.id}
+            movieKey={foreignFilmRes.id}
+            cardClass={"movieMatchForeignCard"}
+            imgClass={"imgContainer"}
+            movieOgLang={foreignFilmRes.original_language}
+            movieTitle={foreignFilmRes.title}
+            moviePoster={foreignFilmRes.poster_path}
+          />
+        </ul>
+      </div>
+      : 
+      <>
+        <h3>Saved!</h3>
+        
+        <Link className="linkButton" to={`/movie/${movieID}`}>Choose another recommendation for this film</Link>
+        <Link className="linkButton" to={'/watchlist'}>View this match among others in the public match list</Link>
+
+      </>
   );
 };
 export default MovieMatch;
+      
+            
+            
