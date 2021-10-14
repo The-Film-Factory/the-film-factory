5d162a682024f133d476a7e02cc1bac79563f237
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-10-13 22:23:52 -0400

    cleanup

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,38 +1,41 @@
 import EnglishMovieSearch from "./EnglishMovieSearch.js";
 import logo from "./assets/logo512.png";
 import { Link } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 
 const Navigation = () => {
-    const [bannerMovieVisibile, setBannerMovieVisible] = useState(false);
+  const [bannerMovieVisibile, setBannerMovieVisible] = useState(false);
 
-    // This function sets the visibility of the dropdown menu and banner movie, which we want to turn off on the homepage
-    // It's disabled when the logo is clicked, and is passed as a prop into the search componentn
-    //======================================================
-    const toggleBannerVisibility = function(toggle){
-        if (toggle === true){
-            setBannerMovieVisible(true)
-        } else {
-            setBannerMovieVisible(false);
-        }
+  // This function sets the visibility of the dropdown menu and banner movie, which we want to turn off on the homepage
+  // It's disabled when the logo is clicked, and is passed as a prop into the search componentn
+  //======================================================
+  const toggleBannerVisibility = function (toggle) {
+    if (toggle === true) {
+      setBannerMovieVisible(true);
+    } else {
+      setBannerMovieVisible(false);
     }
+  };
 
   return (
     <>
       <nav>
         <ul className="navigationContainer">
-            <Link 
-                exact to={`/`}
-                onClick={function(){
-                    setBannerMovieVisible(false)
-                }}    
-            >
-                <li className="logoContainer">
-                    <img className="logo" src={logo} alt="The Film Factory logo" />
-                </li>
-            </Link>
-            <li className='navFilmFactory'>The Film Factory</li>
-            <EnglishMovieSearch toggleBanner={toggleBannerVisibility} bannerMovieVisibile={bannerMovieVisibile}/>
+          <Link
+            to={`/`}
+            onClick={function () {
+              setBannerMovieVisible(false);
+            }}
+          >
+            <li className="logoContainer">
+              <img className="logo" src={logo} alt="The Film Factory logo" />
+            </li>
+          </Link>
+          <li className="navFilmFactory">The Film Factory</li>
+          <EnglishMovieSearch
+            toggleBanner={toggleBannerVisibility}
+            bannerMovieVisibile={bannerMovieVisibile}
+          />
         </ul>
       </nav>
       {/* <h1>The Film Factory</h1> */}
